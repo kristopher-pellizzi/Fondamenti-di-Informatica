6ea42bd7d001fddd8c63d79e50ecd8ccdf287c83
kristopher-pellizzi <krijojo@gmail.com>
2016-01-12 20:30:21 +0100

    Finished exercise

--- espressioni.c
+++ espressioni.c
@@ -5,9 +5,27 @@
 int solve(char* expr){
 	char* sep;
 	char* token;
+	char* expr_cp;
+	int k=0,i=0,cont=0;
 	int res=0,tmp=0;
+	while(expr[i]!='\0'){
+		if(expr[i]=='-')
+			cont++;
+	i++;
+	}
+	expr_cp=malloc(sizeof(char)*(strlen(expr)+cont));
+	i=0;
+	while(expr[i]!='\0'){
+		if(expr[i]=='-'){
+			expr_cp[k++]='+';
+			expr_cp[k++]='-';
+			}
+		else
+			expr_cp[k++]=expr[i];
+		i++;
+	}
 	sep="+";
-	token=strtok(expr,sep);
+	token=strtok(expr_cp,sep);
 	while(token!=NULL){
 		tmp=atoi(token);
 		res+=tmp;
@@ -17,7 +35,7 @@ int solve(char* expr){
 }
 
 int main(){
-	char expr[]="3+2+1";
+	char expr[]="-243+20+1234+432-123454235+182738947";
 	int res=0;
 	res=solve(expr);
 	printf("%d\n",res);
